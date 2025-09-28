652f67037a5848582b51f11d9107a7801316fe68
Lucas Borges <lucasborgesribeiro198@gmail.com>
2025-09-28 20:20:36 -0300

    refactor: altera variaveis para usar dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-const { rejects } = require('assert')
+require('dotenv').config({ path: '.env' });
 const axios = require('axios')
+
+const PROTOCOL1 = process.env.PROTOCOL1
+const BASE_URL1 = process.env.BASE_URL1
+const APP_KEY = process.env.APP_KEY
+
+const PROTOCOL2 = process.env.PROTOCOL2
+const BASE_URL2 = process.env.BASE_URL2
+
+const q = process.env.q
+const BASE_URL3 = process.env.BASE_URL3
+const PROTOCOL3 = process.env.PROTOCOL3
+const API_NEWS_KEY = process.env.API_NEWS_KEY
 // params endpoint 1
-const PROTOCOL1 = 'http'
-const BASE_URL1 = 'api.openweathermap.org/geo/1.0/direct'
-const APP_KEY = '987c46d87ddf0a05f252a83a0358be9c'
-const q = 'Chicago'
 
 const URL = `${PROTOCOL1}://${BASE_URL1}?q=${q}&appid=${APP_KEY}`
 
@@ -12,15 +20,15 @@ const promiseResultante = axios.get(URL);
 
 // promise primeiro endpoint - lat e long
 function LatLong() {
-  return new Promise((resolve, reject) => {
+  return new Promise((req, res) => {
     const response = promiseResultante;
     response.then((response) => {
       const lat = response.data[0].lat;
       const lon = response.data[0].lon;
-      resolve({ lat, lon });
+      req({ lat, lon });
     })
       .catch((err) => {
-        reject(err)
+        res(err)
       })
   });
 }
@@ -29,21 +37,18 @@ LatLong().then((response) => {
   const lat = response.lat;
   const lon = response.lon;
   // params endpoint 2
-  const PROTOCOL2 = 'https'
-  const BASE_URL2 = 'api.openweathermap.org/data/2.5/weather'
-
   const URL2 = `${PROTOCOL2}://${BASE_URL2}?lat=${lat}&lon=${lon}&appid=${APP_KEY}`
 
   const promiseResultante2 = axios.get(URL2);
   // promise segundo endpoint - condicoes
   function conditions() {
-    return new Promise((resolve, reject) => {
+    return new Promise((req, res) => {
       const response = promiseResultante2;
       response.then((response) => {
-        resolve([response.data.main.feels_like, response.data.weather[0].description]);
+        req([response.data.main.feels_like, response.data.weather[0].description]);
       })
         .catch((err) => {
-          reject(console.log(err));
+          res(console.log(err));
         })
     })
   };
@@ -55,22 +60,19 @@ LatLong().then((response) => {
       console.log(err);
     }
   };
-  // promise terceiro endpoint
-  getConditions().then((response) => {
-    const PROTOCOL3 = 'https'
-    const API_NEWS_KEY = '57148d8e1411405090a7bef760d579aa';
-    const NEW_URL = `${PROTOCOL3}://newsapi.org/v2/top-headlines?q=${q}&apiKey=${API_NEWS_KEY}`;
-
+  // promise terceiro endpointBASE_URL3
+  getConditions().then(() => {
+    const NEW_URL = `${PROTOCOL3}://${BASE_URL3}?q=${q}&apiKey=${API_NEWS_KEY}`;
     const promiseResultante3 = axios.get(NEW_URL);
 
     function News() {
-      return new Promise((resolve, reject) => {
+      return new Promise((req, res) => {
         const reponse = promiseResultante3;
         reponse.then((response) => {
-          resolve(response);
+          req(response);
         })
         .catch((err) => {
-          reject(err);
+          res(err);
         });
       });
     }
